8700229252c5ed9f568eb8599b46b642ef8ca955
Dmitry Skvortsov <NickBox99@inbox.ru>
2020-07-24 15:53:50 +0400

    Добавил правную прокрутку для элементов меню и кнопки ВВЕРХ

--- js/scripts.js
+++ js/scripts.js
@@ -25,25 +25,45 @@ const listPhone = () => {
 
 };
 
-//Бургер меню
+//плавная прокрутка до индетификатора
+const smoothScroll = (identifier) => {
+    const blockID = identifier.substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+};
+
+//Бургер меню и плавная прокрутка пунктов меню и кнопки вверх
 const burgetMenu = () => {
     const btnMenu = document.getElementsByClassName("menu__icon")[0],
       popupMenu = document.getElementsByClassName("popup-dialog-menu")[0],
-      btnClose = document.getElementsByClassName("close-menu")[0];
+      btnClose = document.getElementsByClassName("close-menu")[0],
+      btnFooter = document.getElementsByClassName("button-footer")[0];
 
     
     document.addEventListener("click", (event) => {
         const target = event.target;
-        console.log('target: ', target);
-        console.log(target.closest(".popup-menu"));
+
+        const scroll = (href) => {
+            event.preventDefault();
+            smoothScroll(href);
+            popupMenu.removeAttribute("style");
+        };
+
         if (target === btnMenu){
             popupMenu.style.transform = "translate3d(0, 0, 0)";
         }else if(target === btnClose){
             popupMenu.removeAttribute("style");
+        }else if(target.closest(".popup-menu-nav__item")){
+            scroll(target.getAttribute("href"));
+        }else if (target.closest("." + btnFooter.classList[0])) {
+            scroll(btnFooter.querySelector("a").getAttribute("href"));
         }
           
     });
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
     //Список телефонов
