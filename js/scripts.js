d3eaa94acb547ec6dfbcf53157c5e134ede9cef6
Dmitry Skvortsov <NickBox99@inbox.ru>
2020-07-28 00:52:21 +0400

    json

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,8 @@ const modalAddEventListener = () => {
     //Окно благодарности
     popupThank = document.querySelector(".popup-thank");
     
+  let sliderForRepair;
+  let sliderForRepairCarousel;
 
   document.addEventListener("click", (event) => {
     const target = event.target;
@@ -91,6 +93,83 @@ const modalAddEventListener = () => {
     ) {
       popupMenu.removeAttribute("style");
       pupupRepair.style.visibility = "visible";
+
+      fetch("db/db.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          delete sliderForRepair;
+          delete sliderForRepairCarousel;
+
+          const ulListLeft = pupupRepair.querySelector(".nav-list-popup-repair"),
+          ulListRight = pupupRepair.querySelector(".popup-repair-types-content-table");
+          ulListLeft.innerHTML = '';
+          ulListRight.innerHTML = '';
+          
+          data.forEach((element)=>{
+            if(element.date){
+              pupupRepair.querySelector(".popup-repair-types-content__head-date .date").innerText = element.date;
+            }
+            if (element.title) {
+              const newElementLeft = document.createElement("button");
+              newElementLeft.classList.add("button_o", "popup-repair-types-nav__item");
+              newElementLeft.textContent = element.title;
+
+              ulListLeft.append(newElementLeft);
+
+              const newElementRight1 = document.createElement("table");
+              newElementRight1.classList.add("popup-repair-types-content-table__list");
+
+              const newElementRight2 = document.createElement("tbody");
+              newElementRight1.append(newElementRight2);
+
+              element.priceList.forEach((el)=>{
+                const newElementRight3 = document.createElement("tr");
+                newElementRight3.classList.add("mobile-row");
+
+                const text = (el.units === "м2") ? "м<sup>2</sup>" : el.units;
+                newElementRight3.innerHTML = `
+                <td class="repair-types-name">${el.typeService}</td>
+                <td class="mobile-col-title tablet-hide desktop-hide">Ед.измерения</td>
+                <td class="mobile-col-title tablet-hide desktop-hide">Цена за ед.</td>
+                <td class="repair-types-value">${text}</td>
+                <td class="repair-types-value">${el.cost} руб.</td>
+                `;
+
+                newElementRight2.append(newElementRight3);
+              });
+
+              ulListRight.append(newElementRight1);
+
+              //Слайдер
+              sliderForRepair = new Slider({
+                containerId: "popup-repair-types",
+                slidersSelector: ".popup-repair-types-content-table__list",
+                bottons: ".nav-list > button",
+                display: "block",
+                activeClassToButton: "active",
+              });
+              sliderForRepair.init();
+
+              sliderForRepairCarousel = new Slider({
+                containerId: "popup-repair-types",
+                slidersSelector: ".nav-list > button",
+                arrowLeft: "#nav-arrow-popup-repair_left",
+                arrowRight: "#nav-arrow-popup-repair_right",
+                type: "transform",
+              });
+              sliderForRepairCarousel.init();
+            }
+          });
+        })
+        .catch((error) => {
+          console.error("Ошибка при загрузке файла: " + error);
+        });
+    }
+    //Клик по элементам меню 
+    else if(target.closest(".popup-repair-types-nav__item")){
+      document.getElementById("switch-inner").textContent = target.textContent;
     }
     //закрыть модальное окно "Полный список услуг и цен"
     else if (
@@ -567,11 +646,6 @@ const tempFunc = () =>{
       counterMax: ".slider-counter-content__total",
     }).init();
 
-    //P.s кнопку прожать не могу, этот коментарий уберу в финальной версии
-    // setInterval(function () {
-    //   // таймер-планировщик
-    //   document.getElementById("portfolio-arrow-mobile_right").click(); // вызвать клик на кнопку
-    // }, 2000); // через две секунды
   }
 
   //Слайдер для модального окна: Портфолио
@@ -808,11 +882,6 @@ const tempFunc = () =>{
       display: "flex",
     }).init();
   }
-}
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  tempFunc(); //временная функция
 
   //Табы 2
   let sliderSchemeModal = new Slider({
@@ -894,7 +963,6 @@ document.addEventListener("DOMContentLoaded", () => {
       phone = form.querySelector(".feedback__input-input"),
       checkbox = form.querySelector(".checkbox__input");
 
-      console.log("checkbox.checked: ", checkbox.checked);
     if (phone.value.length !== 18){
       phone.style.color = "red";
       return false;
@@ -906,11 +974,20 @@ document.addEventListener("DOMContentLoaded", () => {
     return true;
   }
 
-
   sendForm("feedback1", checker);
   sendForm("feedback2", checker);
   sendForm("feedback3", checker);
   sendForm("feedback4", checker);
   sendForm("feedback5", checker);
   sendForm("feedback6", checker);
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  tempFunc(); //временная функция
+
+  
+
+
+  
 });
